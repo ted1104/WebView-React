805b07117b7d0c907835dd59436836ef346a0715
Teddy Walter <teddywalter2016@gmail.com>
2022-07-03 05:49:25 +0200

    Pi

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>TEST</Text>
+    </View>
+  );
+};
+export default App;
