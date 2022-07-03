48da526c88b515e7c96ec3e77ded8eb61dd2375f
Teddy Walter <teddywalter2016@gmail.com>
2022-07-03 11:05:20 +0200

    done

--- App.js
+++ App.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import {View, Text} from 'react-native';
-
+import React, {Component, useEffect, useState} from 'react';
+import {useNetInfo} from '@react-native-community/netinfo';
+import {View, Text, Image, ActivityIndicator} from 'react-native';
+import {WebView} from 'react-native-webview';
+import {images} from './src/constants';
+const URL = 'https://event.sportpay.bi/agent';
 const App = () => {
+  const netInfo = useNetInfo();
+  const [isConnected, setIsConnected] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setIsConnected(netInfo.isInternetReachable);
+  }, [netInfo]);
+  return isConnected ? (
+    <WebView
+      onLoad={() => console.log('onload')}
+      onLoadStart={() => console.log('onload start')}
+      onError={() => {}}
+      onLoadEnd={() => console.log('onload end')}
+      source={{uri: URL}}
+    />
+  ) : (
+    <NoInternetConnexion />
+  );
+};
+
+const NoInternetConnexion = () => {
   return (
-    <View>
-      <Text>TEST</Text>
+    <View
+      style={{
+        backgroundColor: 'white',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Image source={images.no_connexion} resizeMode="contain" />
+      <Text style={{color: 'red', fontWeight: 'bold'}}>
+        Veuillez verifier votre connexion internet!
+      </Text>
     </View>
   );
 };
---
+++ src/constants/images.js
@@ -0,0 +1,7 @@
+const no_connexion = require('../res/assets/images/connexion.png');
+
+const images = {
+  no_connexion,
+};
+
+export default images;
